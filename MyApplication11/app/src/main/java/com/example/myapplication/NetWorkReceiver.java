f7a26c2f128b97ed12370658e2f2ff0452650790
vanhai2000 <0905190166h@gmail.com>
2020-10-09 10:27:28 +0700

    lab4

--- MyApplication11/app/src/main/java/com/example/myapplication/NetWorkReceiver.java
+++ MyApplication11/app/src/main/java/com/example/myapplication/NetWorkReceiver.java
@@ -12,15 +12,22 @@ public class NetWorkReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (MainActivity.WIFI.equals(MainActivity.sPref) && networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+        NetworkInfo networkInfo1 = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean mobile = networkInfo1.isConnected();
+
+        if (MainActivity.WIFI.equals(MainActivity.sPref) && networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI && mobile) {
             MainActivity.refreshDisplay = true;
-            Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, R.string.all, Toast.LENGTH_SHORT).show();
         } else if (MainActivity.ANY.equals(MainActivity.sPref) && networkInfo != null) {
             MainActivity.refreshDisplay = true;
-        } else {
+        } else if (MainActivity.WIFI.equals(MainActivity.sPref) && networkInfo != null && networkInfo.getType() != ConnectivityManager.TYPE_WIFI && mobile){
             MainActivity.refreshDisplay = false;
-            Toast.makeText(context, R.string.lost_connected, Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(context, "Chua ket noi Wifi!", Toast.LENGTH_SHORT).show();
+        } else if(MainActivity.WIFI.equals(MainActivity.sPref) && networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI && !mobile){
+            Toast.makeText(context, "chua ket noi 3G!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Khong co ket noi nao!", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
